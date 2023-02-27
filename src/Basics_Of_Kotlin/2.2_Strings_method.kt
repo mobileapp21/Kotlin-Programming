288ad49070469440b5459d29ce835ef9a80edf5f
Coder <sa3dideveloper21@gmail.com>
2023-02-27 11:57:20 -0800

    Kotlin String method 0.1

---
+++ src/Basics_Of_Kotlin/2.2_Strings_method.kt
@@ -0,0 +1,14 @@
+package Basics_Of_Kotlin
+
+
+
+fun main(){
+    val nameSurname = "  Halil Ozel   "
+
+    println(nameSurname) //   Halil Ozel
+
+    // trim()  : This method deletes spaces.
+    println(nameSurname.trim()) // Halil Ozel
+
+
+}
