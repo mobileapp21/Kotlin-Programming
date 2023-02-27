2121281697841d9342651f86f8f18431d7d3c047
Coder <saadizebary@gmail.com>
2023-02-27 10:47:56 -0800

    Kotlin Hello World Program

---
+++ src/HelloWorldProgram.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Hello World!") // The println() function prints the given message inside the quotation marks
+    // and newline to the standard output stream.
+
+    print("We Love Kotlin") // Prints the given message to the standard output stream. print() has not newline
+}
