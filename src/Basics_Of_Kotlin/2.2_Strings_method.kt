e68646a572c6bd98f243bab1322475b278e6477d
Coder <sa3dideveloper21@gmail.com>
2023-02-27 12:02:52 -0800

    Kotlin String method 0.3

--- src/Basics_Of_Kotlin/2.2_Strings_method.kt
+++ src/Basics_Of_Kotlin/2.2_Strings_method.kt
@@ -16,5 +16,15 @@ fun main(){
 
     // split() : This method divides
     println(tvSeries.split(" ")) // [How, I, Met, Your, Mother]
+// When it finds the letter r, it divides it up.
+    println(tvSeries.split("r")) // [How I Met You,  Mothe, ]
 
+    // When it finds the letter o, it divides it up. New Array is ready
+    var newSeries = tvSeries.split("o")
+
+    println("newSeries : $newSeries") // newSeries : [H, w I Met Y, ur M, ther]
+
+    println("newSeries Size : ${newSeries.size}") // newSeries Size : 4
+
+    println("newSeries[2] = ${newSeries[2]}") // newSeries[2] = ur M
 }
