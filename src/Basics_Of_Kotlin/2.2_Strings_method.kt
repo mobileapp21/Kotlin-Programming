ba2efa545ab36caa9613c2f412cc1ece98f7d564
Coder <sa3dideveloper21@gmail.com>
2023-02-27 11:58:29 -0800

    Kotlin String method 0.2

--- src/Basics_Of_Kotlin/2.2_Strings_method.kt
+++ src/Basics_Of_Kotlin/2.2_Strings_method.kt
@@ -11,4 +11,10 @@ fun main(){
     println(nameSurname.trim()) // Halil Ozel
 
 
+    // String Value
+    var tvSeries = "How I Met Your Mother"
+
+    // split() : This method divides
+    println(tvSeries.split(" ")) // [How, I, Met, Your, Mother]
+
 }
