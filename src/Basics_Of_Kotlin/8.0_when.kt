c9d19104f3d5e6d7895706037275dbb970cbf32f
Coder <sa3dideveloper21@gmail.com>
2023-02-28 07:42:48 -0800

    Kotlin Loops

--- src/Basics_Of_Kotlin/8.0_when.kt
+++ src/Basics_Of_Kotlin/8.0_when.kt
@@ -1,2 +1,18 @@
 package Basics_Of_Kotlin
 
+
+fun main(){
+    val day = 1
+
+    val result = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Invalid day."
+    }
+    println(result)
+}
