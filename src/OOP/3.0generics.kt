0c32df11d3850155dc294251ec123b01fff643b1
Coder <sa3dideveloper21@gmail.com>
2023-03-04 02:50:57 -0800

    Kotlin enum

--- src/OOP/3.0generics.kt
+++ src/OOP/3.0generics.kt
@@ -10,5 +10,5 @@ fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
     val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
     val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
-print(question1)
+print(question1.questionText)
 }
